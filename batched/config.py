6c965e2f735f053b1d9fdd2fe448f581d9260cd8
Justin Russell <jrussell9000@gmail.com>
2024-09-04 15:58:40 -0500

    Reorg modules directory

--- batched/config.py
+++ batched/config.py
@@ -1,14 +1,18 @@
-aws_access_key_id: str = 'AKIAQMJTZW6JWUAVZ7K3'
-aws_secret_access_key: str = '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'
+import os
+
+
+aws_access_key_id: str = os.environ['AWS_ACCESS_KEY_ID']
+aws_secret_access_key: str = os.environ['AWS_SECRET_ACCESS_KEY']
 aws_s3bucket: str = "brc-abcd"
 aws_region: str = "us-east-1"
 batch_size: int = 10
-nda_username: str = 'jrusse10'
-nda_password: str = '19Ireland61'
+nda_username: str = os.environ['NDA_USERNAME']
+nda_password: str = os.environ['NDA_PASSWORD']
 miNDAR_packageID: str = '1228348'
-miNDAR_password: str = 'Stella91'
+miNDAR_password: str = os.environ['MINDAR_PASSWORD']
 miNDAR_username: str = "_".join([nda_username, miNDAR_packageID])
-miNDAR_host = "mindarvpc.cqahbwk3l1mb.us-east-1.rds.amazonaws.com"
+miNDAR_host: str = os.environ['MINDAR_HOST']
+# miNDAR_host = "mindarvpc.cqahbwk3l1mb.us-east-1.rds.amazonaws.com"
 miNDAR_tablename = "fmriresults01"
 trackingdb_tablename = "FastBatchTracking"
 nThreads: str = "1"
